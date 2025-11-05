62b7bf527bd26797e16650481a36e0e0c05e944d
David Garcia <david_3141240195@utd.edu.mx>
2025-11-05 08:13:02 -0600

    Se agregaron archivos hasta la practica 17

--- P2/INTRODUCCION_TKINTER/7_entry.py
+++ P2/INTRODUCCION_TKINTER/7_entry.py
@@ -1,16 +1,18 @@
 from tkinter import *
 
+"""
 ventana = Tk()
 ventana.title("Entry")
 ventana.geometry("500x500")
 
 def cambiar():
-    nombre= entrada1.get()
-    etiqueta2.config(text=f"Hola {nombre} bienvenido")
+    etiqueta2.config(text=f"Hola {nombre.get()} bienvenido")
+
+nombre=StringVar()
 
 etiqute1=Label(ventana, text="Ingrese su nombre: ").pack()
 
-entrada1=Entry(ventana)
+entrada1=Entry(ventana, textvariable=nombre)
 entrada1.pack()
 
 boton1=Button(ventana,text="Saludar",command= cambiar)
@@ -19,4 +21,70 @@ boton1.pack()
 etiqueta2=Label(ventana, text="")
 etiqueta2.pack()
 
+ventana.mainloop()
+"""
+def entrar():
+    frame_bienvenido.config(
+    width=500, 
+    height=100, 
+    bg="lightblue", 
+    relief=SOLID, 
+    border=2
+    )
+    label_bienvenido.config(
+        text=f"Bienvenido al Sistema {entry_usuario.get()}"
+    )
+
+def borrar():
+    entry_usuario.delete(0, END)
+    entry_contrasena.delete(0, END)
+    label_bienvenido.config(text="")
+    frame_bienvenido.config(
+        width=0, 
+        height=0, 
+        bg="lightblue", 
+        relief=SOLID, 
+        border=2
+    )
+
+ventana = Tk()
+ventana.title("Entry")
+ventana.geometry("500x500")
+
+label_titulo=Label(ventana, text="Acceso al sistema")
+label_titulo.pack()
+label_titulo.config(
+    font=("Arial", 18)
+)
+
+label_usuario=Label(ventana, text="Ingresa tu usuario")
+label_usuario.pack()
+label_usuario.config(
+    font=("Arial", 12)
+)
+
+entry_usuario=Entry(ventana)
+entry_usuario.pack()
+
+label_contrasena=Label(ventana, text="Ingresa tu contraseña")
+label_contrasena.pack()
+label_contrasena.config(
+    font=("Arial", 12)
+)
+
+entry_contrasena=Entry(ventana)
+entry_contrasena.pack()
+
+button_aceptar=Button(ventana, text="Aceptar", command= entrar)
+button_aceptar.pack()
+
+button_borrar=Button(ventana, text="Borrar", command= borrar)
+button_borrar.pack()
+
+frame_bienvenido=Frame(ventana)
+frame_bienvenido.pack(pady=10)
+
+label_bienvenido=Label(frame_bienvenido, text="")
+label_bienvenido.pack(pady=25)
+#text="Bienvenido al Sistema"
 ventana.mainloop()
