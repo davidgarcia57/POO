7ae25f1d20691c9a6c05adebfecae72c17a3ce75
David Garcia <david_3141240195@utd.edu.mx>
2025-11-05 09:16:59 -0600

    se agregaron las practicas del dia 5/11/2025

--- P2/INTRODUCCION_TKINTER/10_radioButton.py
+++ P2/INTRODUCCION_TKINTER/10_radioButton.py
@@ -1,7 +1,26 @@
 from tkinter import * 
 
 ventana = Tk()
-ventana.title("radioButton")
+ventana.title("checkButton")
 ventana.geometry("500x500")
+
+def mostrarEstado():
+    resultado.config(text=f"Opción Seleccionada: {opcion.get()}")
+
+opcion = StringVar()
+radioBoton1 = Radiobutton(ventana, text="Opción 1", variable=opcion, value="Opcion1")
+radioBoton1.pack()
+
+radioBoton2 = Radiobutton(ventana, text="Opción 2", variable=opcion, value="Opcion2")
+radioBoton2.pack()
+
+radioBoton3 = Radiobutton(ventana, text="Opción 3", variable=opcion, value="Opcion3")
+radioBoton3.pack()
+
+boton=Button(ventana, text="Mostrar Opción", command=mostrarEstado)
+boton.pack()
+
+resultado=Label(ventana, text="")
+resultado.pack()
 
 ventana.mainloop()
--- P2/INTRODUCCION_TKINTER/11_scale.py
+++ P2/INTRODUCCION_TKINTER/11_scale.py
@@ -4,4 +4,16 @@ ventana = Tk()
 ventana.title("scale")
 ventana.geometry("500x500")
 
+def mostrarEstado():
+    resultado.config(text="El Valo es: ")
+
+barra = Scale(ventana, orient="vertical")
+barra.pack()
+
+boton=Button(ventana, text="Mostrar Valor", command=mostrarEstado)
+boton.pack()
+
+resultado=Label(ventana, text="")
+resultado.pack()
+
 ventana.mainloop()
