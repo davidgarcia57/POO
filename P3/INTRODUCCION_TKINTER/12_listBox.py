a93db9bd0d613c91fd727d941679310d70bf6d04
David Garcia <david_3141240195@utd.edu.mx>
2025-11-07 09:18:44 -0600

    practicas del dia 7/11/2025

--- P3/INTRODUCCION_TKINTER/12_listBox.py
+++ P3/INTRODUCCION_TKINTER/12_listBox.py
@@ -4,4 +4,21 @@ ventana = Tk()
 ventana.title("listBox")
 ventana.geometry("500x500")
 
+def mostrarEstado():
+    seleccion = lista.get(lista.curselection())
+    resultado.config(text=f"Seleccionaste: {seleccion}")
+
+lista=Listbox(ventana, width=10, height=5, selectmode='single')
+lista.pack()
+
+opciones=['Amarillo','Rojo', 'Azul', 'Morado']
+for i in opciones:
+    lista.insert(END,i)
+
+boton=Button(ventana, text="Mostrar seleccion del usuario", command=mostrarEstado)
+boton.pack()
+
+resultado=Label(ventana, text="")
+resultado.pack()
+
 ventana.mainloop()
--- P3/INTRODUCCION_TKINTER/13_menu.py
+++ P3/INTRODUCCION_TKINTER/13_menu.py
@@ -1,7 +1,37 @@
 from tkinter import * 
 
+def mostrarEstado(tipo):
+    if tipo == "nuevo":   
+        resultado.config(text=f"Nuevo Archivo")
+    elif tipo == "guardar":
+        resultado.config(text=f"Guardar Archivo")
+    elif tipo == "copiar":
+        resultado.config(text=f"Archivo Copiado")
+    elif tipo == "recortar":
+        resultado.config(text=f"Archivo recortado")
+
 ventana = Tk()
 ventana.title("menu")
 ventana.geometry("500x500")
+
+menuBar= Menu(ventana)
+ventana.config(menu=menuBar)
+
+archivoMenu=Menu(menuBar, tearoff=False)
+menuBar.add_cascade(label="Archivo", menu=archivoMenu)
+archivoMenu.add_command(label="Nuevo Archivo",command=lambda: mostrarEstado("nuevo"))
+archivoMenu.add_command(label="Guardar Archivo",command=lambda: mostrarEstado("guardar"))
+archivoMenu.add_separator()
+archivoMenu.add_command(label="Salir", command=ventana.quit)
+
+archivoMenu2=Menu(menuBar, tearoff=False)
+menuBar.add_cascade(label="Edición", menu=archivoMenu2)
+archivoMenu2.add_command(label="Copiar",command=lambda: mostrarEstado("copiar"))
+archivoMenu2.add_command(label="Recortar",command=lambda: mostrarEstado("recortar"))
+archivoMenu2.add_separator()
+archivoMenu2.add_command(label="Salir", command=ventana.quit)
+
+resultado=Label(ventana, text="")
+resultado.pack()
 
 ventana.mainloop()
