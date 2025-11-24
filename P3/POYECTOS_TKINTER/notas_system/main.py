3331b0c618dcc0b3f2b208037b9b572a15cecd53
David Garcia <david_3141240195@utd.edu.mx>
2025-11-24 10:56:34 -0600

    Implementar conexión a la base de datos y métodos para gestionar notas y usuarios

---
+++ P3/POYECTOS_TKINTER/notas_system/main.py
@@ -0,0 +1,17 @@
+'''
+1.- Paradigma OO
+2.- Implementar MVC
+3.- App de Escritorio con interfaz gráfica
+'''
+
+from tkinter import *
+
+
+class App:
+    def __init__(self):
+        pass
+
+if __name__=="__main__":
+    ventana=Tk()
+    app=App
+    ventana.mainloop()
