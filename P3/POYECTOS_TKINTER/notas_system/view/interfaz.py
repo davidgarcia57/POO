346bdddc2e5b2ad6558f2a779379566acf7bd567
David Garcia <david_3141240195@utd.edu.mx>
2025-11-26 08:40:02 -0600

    Refactor métodos de la clase Vistas para mejorar la legibilidad y corregir errores tipográficos

--- P3/POYECTOS_TKINTER/notas_system/view/interfaz.py
+++ P3/POYECTOS_TKINTER/notas_system/view/interfaz.py
@@ -5,19 +5,19 @@ class Vistas():
         ventana.title("Gestión de Notas")
         ventana.geometry("500x400")
         ventana.resizable(False,False)
-        self.menuPrincipal(ventana)
+        self.menu_principal(ventana)
 
     def borrarPantalla(self,ventana):
         for widget in ventana.winfo_children():
             widget.destroy()
 
-    def menuPrincipal(self,ventana):
+    def menu_principal(self,ventana):
         self.borrarPantalla(ventana)
         label_titulo=Label(ventana, text=".::Menu principal::.") 
         label_titulo.pack(pady=5)
         label_titulo.config()
 
-        boton_registro=Button(ventana, text="1.-Registro",command=lambda: self.resgistroSistema(ventana))
+        boton_registro=Button(ventana, text="1.-Registro",command=lambda: self.registro(ventana))
         boton_registro.pack(pady=5)
         boton_registro.config()
 
@@ -29,9 +29,9 @@ class Vistas():
         boton_salir.pack(pady=5)
         boton_salir.config()
 
-    def login(self,ventana):
+    def registro(self,ventana):
         self.borrarPantalla(ventana)
-        lbl_titulo=Label(ventana, text=".::Inicio de Sesión::.")
+        lbl_titulo=Label(ventana, text=".::Registro en el Sistema::.")
         lbl_titulo.pack(pady=5)
         lbl_titulo.config()
         
@@ -56,24 +56,76 @@ class Vistas():
         lbl_contrasena=Label(ventana, text="Ingresa tu Contraseña:")
         lbl_contrasena.pack(pady=5)
         lbl_contrasena.config()
-        entry_contrasena=Entry(ventana, width=30)
+        entry_contrasena=Entry(ventana, width=30, show="*")
         entry_contrasena.pack(pady=5)
 
         boton_registrar=Button(ventana, text="Registrar")
         boton_registrar.pack(pady=5)
         boton_registrar.config()
 
-        boton_volver=Button(ventana, text="Volver", command=lambda: self.menuPrincipal(ventana))
+        boton_volver=Button(ventana, text="Volver", command=lambda: self.menu_principal(ventana))
         boton_volver.pack(pady=5)
         boton_volver.config()
 
-    def resgistroSistema(self,ventana):
+    def login(self,ventana):
         self.borrarPantalla(ventana)
-        lbl_titulo=Label(ventana, text=".::Registro en el Sistema::.")
+        lbl_titulo=Label(ventana, text=".::Inicio de Sesión::.")
         lbl_titulo.pack(pady=5)
         lbl_titulo.config()
         
-        lbl_email=Label(ventana, text="Ingresa tu email:")
+        lbl_email=Label(ventana, text="Ingresa tu E-mail:")
+        lbl_email.pack(pady=5)
+        lbl_email.config()
+        entry_email=Entry(ventana, width=30)
+        entry_email.pack(pady=5)
+
+        lbl_contrasena=Label(ventana, text="Ingresa tu Contraseña:")
+        lbl_contrasena.pack(pady=5)
+        lbl_contrasena.config()
+        entry_contrasena=Entry(ventana, width=30, show="*")
+        entry_contrasena.pack(pady=5)
+
+        boton_entrar=Button(ventana, text="Entrar", command=lambda: self.menu_notas(ventana, "David", "Garcia"))
+        boton_entrar.pack(pady=5)
+        boton_entrar.config()
+
+        boton_volver=Button(ventana, text="Volver", command=lambda: self.menu_principal(ventana))
+        boton_volver.pack(pady=5)
+        boton_volver.config()
+
+    def menu_notas(self,ventana,nombre,apellidos):
+        self.borrarPantalla(ventana)
+        label_titulo=Label(ventana, text=f".::Bienvenido {nombre} {apellidos}, has iniciado sesión::.") 
+        label_titulo.pack(pady=5)
+        label_titulo.config()
+
+        boton_crear=Button(ventana, text="1.-Crear",command=lambda: self.crear())
+        boton_crear.pack(pady=5)
+        boton_crear.config()
+
+        boton_mostrar=Button(ventana, text="2.-Mostrar", command=lambda: "")
+        boton_mostrar.pack(pady=5)
+        boton_mostrar.config()
+
+        boton_cambiar=Button(ventana, text="3.-Cambiar", command=lambda: "")
+        boton_cambiar.pack(pady=5)
+        boton_cambiar.config()
+
+        boton_eliminar=Button(ventana, text="4.-Eliminar", command=lambda: "")
+        boton_eliminar.pack(pady=5)
+        boton_eliminar.config()
+
+        boton_regresar=Button(ventana, text="5.-Regresar", command=lambda: self.login(ventana))
+        boton_regresar.pack(pady=5)
+        boton_regresar.config()
+
+    def crear(self,ventana):
+        self.borrarPantalla(ventana)
+        lbl_titulo=Label(ventana, text=".::Crear Nota::.")
+        lbl_titulo.pack(pady=5)
+        lbl_titulo.config()
+        
+        lbl_email=Label(ventana, text="Titulo:")
         lbl_email.pack(pady=5)
         lbl_email.config()
         entry_email=Entry(ventana, width=30)
@@ -85,10 +137,10 @@ class Vistas():
         entry_contrasena=Entry(ventana, width=30)
         entry_contrasena.pack(pady=5)
 
-        boton_entrar=Button(ventana, text="Entrar")
+        boton_entrar=Button(ventana, text="Entrar", command=lambda: self.menu_notas)
         boton_entrar.pack(pady=5)
         boton_entrar.config()
 
-        boton_volver=Button(ventana, text="Volver", command=lambda: self.menuPrincipal(ventana))
+        boton_volver=Button(ventana, text="Volver", command=lambda: self.menu_principal(ventana))
         boton_volver.pack(pady=5)
         boton_volver.config()
